1be4eb50e69d555cafb9cef0697fcf32991ae6ae
moforemmanuel <moforemmanuel1000@gmail.com>
2024-05-24 14:48:20 +0100

    feat(pages): add error page for unsupported devices

---
+++ src/pages/error/unsupported-device.tsx
@@ -0,0 +1,28 @@
+import { AlertCircle } from "lucide-react";
+
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+import React from "react";
+import { useScreenSize } from "@/hooks";
+import { useRouter } from "next/router";
+
+export default function UnsupportedDevice() {
+  const { isXl, isXxl } = useScreenSize();
+  const router = useRouter();
+
+  React.useEffect(() => {
+    if (isXl || isXxl) router.push("/");
+  }, [isXl, isXxl, router]);
+
+  return (
+    <div className="h-screen w-full flex items-center justify-center p-4">
+      <Alert variant="destructive">
+        <AlertCircle className="h-4 w-4" />
+        <AlertTitle>Erreur</AlertTitle>
+        <AlertDescription>
+          Cet appareil n'est pas pris en charge, veuillez utiliser un ordinateur
+          de bureau ou un PC
+        </AlertDescription>
+      </Alert>
+    </div>
+  );
+}
