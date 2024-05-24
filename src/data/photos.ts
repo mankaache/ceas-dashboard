d8e31163ee521d6ddcaaf44bb673de4f5ce6a46a
moforemmanuel <moforemmanuel1000@gmail.com>
2024-05-24 14:46:58 +0100

    add mock data

---
+++ src/data/photos.ts
@@ -0,0 +1,30 @@
+import { commonImages } from "@/assets";
+import { IPhoto } from "@/models";
+
+export const PHOTOS: IPhoto[] = [
+  {
+    src: commonImages.user.src,
+    caption: "Lorem Ipsum",
+    status: "active",
+    createdAt: "2014-07-12 10:42 AM",
+  },
+  {
+    src: commonImages.user.src,
+    caption: "Lorem Ipsum",
+    status: "draft",
+    createdAt: "2014-07-12 10:42 AM",
+  },
+  {
+    src: commonImages.user.src,
+    caption:
+      "Lorem Ipsum si possui un nu pe at vero eu fugiat nulla consequ AAA A   la rebum ab illo er at vero eu",
+    status: "draft",
+    createdAt: "2014-07-12 10:42 AM",
+  },
+  {
+    src: commonImages.user.src,
+    caption: "Lorem Ipsum",
+    status: "active",
+    createdAt: "2014-07-12 10:42 AM",
+  },
+];
