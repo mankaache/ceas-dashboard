eef947c412ef9412b119d4b0b0efa499e1599394
moforemmanuel <moforemmanuel1000@gmail.com>
2024-06-13 08:55:38 +0100

    update media and category models

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,2 +1,9 @@
 export * from "./auth";
 export * from "./media";
+
+export interface ICategory {
+  label: string;
+  value: string;
+  createdAt: string;
+  modifiedAt: string;
+}
--- src/models/media.ts
+++ src/models/media.ts
@@ -6,6 +6,7 @@ export interface IPhoto {
   status: "active" | "draft";
   createdAt: string;
   modifiedAt?: string;
+  category: string;
 }
 
 export interface IVideo {
@@ -15,6 +16,7 @@ export interface IVideo {
   status: "active" | "draft";
   createdAt: string;
   modifiedAt?: string;
+  category: string;
 }
 
 export interface IDocument {
@@ -24,4 +26,33 @@ export interface IDocument {
   status: "active" | "draft";
   createdAt: string;
   modifiedAt?: string;
+  category: string;
+}
+
+export interface IArticle {
+  slug: string;
+  image: { src: string; alt: string };
+  title: string;
+  excerpt: string;
+  content: string;
+  tags: string[];
+  author: { name: string };
+  date: string;
+  status: "active" | "draft";
+}
+
+export interface IEvent {
+  title: string;
+  slug: string;
+  id?: string;
+  image: {
+    src: string;
+    alt: string;
+  };
+  date: string;
+  category: string;
+  excerpt: string;
+  content: string;
+  location: string;
+  status: "active" | "draft";
 }
